8e27de053a1198699bbff68200a7cd430c7d2741
JIEUN <jsilver0127@gmail.com>
2025-01-13 00:41:14 +0900

    Jan 12th

---
+++ Practice/src/SWEA/D1/최대수구하기.java
@@ -0,0 +1,11 @@
+package SWEA.D1;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class 최대수구하기 {
+    public static void main(String[] args) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    }
+}
--- Practice/src/SWEA/D2/중간평균값구하기.java
+++ Practice/src/SWEA/D2/중간평균값구하기.java
@@ -1,11 +1,35 @@
 package SWEA.D2;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class 중간평균값구하기 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int tc = Integer.parseInt(br.readLine());
+        for(int t = 1; t <= tc; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
+            int ans = 0;
+            int max = 0;
+            int min = Integer.MAX_VALUE;
+
+            for (int i = 0; i < 10; i++) {
+                int n = Integer.parseInt(st.nextToken());
+                ans += n;
+                max = Math.max(max, n);
+                min = Math.min(min, n);
+            }
+
+            ans -= max;
+            ans -= min;
+
+            double average = ans / 8.0;
+            int roundedAvarage = (int)Math.round(average);
+
+            System.out.println("#" + t + " " + roundedAvarage);
+        }
     }
 }
